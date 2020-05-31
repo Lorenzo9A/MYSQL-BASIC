412e011b5b5de33c362fdf98b025649bc33d2d65
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-31 13:18:15 +0200

    taak 1 delete af

--- 2-Select/taak04/antwoorden.sql
+++ 2-Select/taak04/antwoorden.sql
@@ -1,7 +1,11 @@
 -- Opdracht 1 
-
+SELECT Max(wage) As hoogste_loon_speler_fc_utrecht
+FROM players
+WHERE club = "FC Utrecht"
 -- Opdracht 2 
-
+SELECT AVG(wage), ROUND(AVG(wage)) AS avg_wage
+FROM players
+WHERE wage
 -- Opdracht 3
 
 -- Opdracht 4
